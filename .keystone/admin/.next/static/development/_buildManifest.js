6ad58ed048d130ea5b46ec6f794cd52e6f8bd7df
ZsokaM <zsokamontvai@gmail.com>
2021-08-08 22:34:35 +0200

    gitignore should work now

--- .keystone/admin/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/sport-classes":["static\u002Fchunks\u002Fpages\u002Fsport-classes.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fsport-classes"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
